215a64f9227a84a5c2714bb4dcf837d933c275d7
Gabriel Hernandez <gabhernandez@hotmail.ca>
2024-12-04 13:11:01 -0500

    working on a more advanced look for the project section, when the user clicks on a project it will load the project i did instead of a basic page where all the prohjects are already listed

--- script.js
+++ script.js
@@ -12,6 +12,7 @@ function changeContent(page) {
 					<li><strong>NetBeans</strong></li>
 					<li><strong>Linux</strong></li>
 					<li><strong>Kali Linux</strong></li>
+					<li><strong>Oracle</strong></li>
 					<ul>
 				</p>
                 <p> I decided to make a career change in August 2024 in the coding/cybersecurity world in August 2024. I am learning most of my skills through hands-on project via the Computer Programming Course at Humber College, YoutTube videos, and tutorials.
@@ -23,23 +24,22 @@ function changeContent(page) {
 			break;
 		case 'about':
 			contentDiv.innerHTML = `
+				<h2>About My Projects</h2>
+				<ul>
+				<li><a href="#" on click="toggleProjectDetails(1)">Project 1: Created a simple webpage using HTML. Applied the basic HTML structure, use essential HTML tags, and include various elements such as headings, paragraphs, links, images, lists, and tables.</a></li>
+				<li><a href="#" on click="toggleProjectDetails(2)">Project 2: Created a simple web page that allows users to input their name, save it locally using the HTML5 Web Storage API, and retrieve it whenever they visit the page. Interacted with the Local Storage feature in HTML5.</a></li>
+				<li><a href="#" on click="toggleProjectDetails(3)">Project 3: Created a blog website with a homepage that displays a list of blog posts, a page to create new posts, and the ability to edit or delete existing posts. Learned Git and GitHub with proper version control workflows.</a></li>
+				</ul>
+				<div id="ProjectDetails"></div>
 				<p>
-                Project 1: <br>
-                Created a simple webpage using HTML. Applied the basic HTML structure, use essential HTML tags, and include various elements such as headings, paragraphs, links, images, lists, and tables.
+				<a href="#" onclick="changeContent('skills')">Check out my skills</a>
 				</p>
-				<p>
-                Project 2: <br>
-                Created a simple web page that allows users to input their name, save it locally using the HTML5 Web Storage API, and retrieve it whenever they visit the page. Interacted with the Local Storage feature in HTML5.
-				</p>
-                <p>
-                Project 3: <br>
-                Created a blog website with a homepage that displays a list of blog posts, a page to create new posts, and the ability to edit or delete existing posts. Learned Git and GitHub with proper version control workflows.
-                </p>
 			`;
 			break;
-		case 'contact':
-			contentDiv.innerHTML = 
-				` 
+		case 'skills':
+			contentDiv.innerHTML = ` 
+			    <h2>Skills</h2>
+				
 				<p>
 					Feel free to reach out!
 				</p> 
